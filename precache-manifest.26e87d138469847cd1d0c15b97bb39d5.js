4015863dc7615625e776ec4f7cd30df58024f77a
pengyongjia8954 <2338787406@qq.com>
2025-03-23 17:05:17 +0800

    Add files via upload

---
+++ precache-manifest.26e87d138469847cd1d0c15b97bb39d5.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "523a36cc6e5b98b0e67b",
+    "url": "/fake-yiban/dist/css/app.247be998.css"
+  },
+  {
+    "revision": "facbd5bf46d58d08609efff69c458db1",
+    "url": "/fake-yiban/dist/img/about.facbd5bf.svg"
+  },
+  {
+    "revision": "d47d282be2990d70b08bd0b498a2a082",
+    "url": "/fake-yiban/dist/img/add.d47d282b.svg"
+  },
+  {
+    "revision": "bade37cb279c36d23f3521b94c2fd4b8",
+    "url": "/fake-yiban/dist/img/back.bade37cb.svg"
+  },
+  {
+    "revision": "b2ffde2613599160449d0362bb69ccac",
+    "url": "/fake-yiban/dist/img/close.b2ffde26.svg"
+  },
+  {
+    "revision": "b6bbbf94adac88238b44ee4cdb29c3ec",
+    "url": "/fake-yiban/dist/img/delete.b6bbbf94.svg"
+  },
+  {
+    "revision": "15eb1df8207605e82523c19b44520aed",
+    "url": "/fake-yiban/dist/img/logo.15eb1df8.png"
+  },
+  {
+    "revision": "c6c92fec7fe8ef49f66a014c25049ab2",
+    "url": "/fake-yiban/dist/img/menu.c6c92fec.svg"
+  },
+  {
+    "revision": "ccd4d4ec6bbf252b23160975a2813d17",
+    "url": "/fake-yiban/dist/img/qrcode0.ccd4d4ec.svg"
+  },
+  {
+    "revision": "4f42d1ee19c69626e0744435c0b261a4",
+    "url": "/fake-yiban/dist/img/qrcode1.4f42d1ee.svg"
+  },
+  {
+    "revision": "a22700c52873ef89f256d6e28a04397f",
+    "url": "/fake-yiban/dist/img/star_back.a22700c5.png"
+  },
+  {
+    "revision": "b55eb5f55e339c59bc201111170fe3cf",
+    "url": "/fake-yiban/dist/index.html"
+  },
+  {
+    "revision": "523a36cc6e5b98b0e67b",
+    "url": "/fake-yiban/dist/js/app.2414cbfa.js"
+  },
+  {
+    "revision": "dca02ef2d0d9887a3821",
+    "url": "/fake-yiban/dist/js/chunk-vendors.123a1cbc.js"
+  },
+  {
+    "revision": "d962cad20740c8cc61c4814769cf730c",
+    "url": "/fake-yiban/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fake-yiban/dist/robots.txt"
+  }
+]);
